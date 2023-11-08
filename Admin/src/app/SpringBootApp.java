999d9ecc3373681bdf8f89f33f2257c255821153
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-11-08 14:57:57 +0800

    created admin app

---
+++ Admin/src/app/SpringBootApp.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+// this will disable the DB initialization which we do not need at this point
+public class SpringBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp.class, args);
+	}
+}
