26d148704c256e3ce1fcadd5b7ba0d814a9e554a
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-11-28 17:09:07 +0800

    connected creat reservee account on frontend

--- backend/ReserveeApplication/src/app/rest/controllers/ReserveeController.java
+++ backend/ReserveeApplication/src/app/rest/controllers/ReserveeController.java
@@ -1,10 +1,7 @@
 package app.rest.controllers;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,18 +9,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import app.components.ReserveeComponent;
 import app.entities.Reservee;
+import javax.ws.rs.OPTIONS;
 
 
 @Path("/reservee")
@@ -33,20 +25,44 @@ public class ReserveeController {
     @Autowired
     private ReserveeComponent rComp;
 
-    @Transactional
-    @PostMapping(path = "createaccount")
-    public String createAccount(Reservee reservee) {
-        return rComp.create(reservee);
+    @OPTIONS
+    @Path("/createaccount")
+    public Response optionsForCreateAccount() {
+        return Response.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "POST, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+                .build();
     }
-  
+    
+    @POST
+    @Path("/createaccount")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @CrossOrigin(origins = {"http://localhost:3000", "http://172.20.10.5:3000", "http://localhost:9997"})
+    public Response createAccount(Reservee reservee) {
+        try {
+        	return Response.ok(rComp.create(reservee))
+        			   .header("Access-Control-Allow-Origin", "*")
+        			   .header("Access-Control-Allow-Methods", "POST, OPTIONS")
+        			   .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        			   .build();
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
     @GET
 	@Path("/view")
 	@Produces(MediaType.APPLICATION_JSON)    
     @CrossOrigin(origins = {"http://localhost:3000", "http://172.20.10.5:3000", "http://localhost:9997"})
     public Response verifyReservee(@QueryParam("reserveeID") Long reserveeID) {
         try {
-            return Response.ok(rComp.verifyReservee(reserveeID)).header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            return Response.ok(rComp.verifyReservee(reserveeID))
+            		.header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Methods", "GET, OPTIONS")
                     .header("Access-Control-Allow-Headers", "Content-Type")
                     .build();
         } catch (Exception e) {
