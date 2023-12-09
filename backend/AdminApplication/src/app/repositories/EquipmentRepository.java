842263e94c19893a9fd156cb3a51bc17a963d125
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-12-09 14:36:20 +0800

    removed equipment entity and set it to equipment request

--- backend/AdminApplication/src/app/repositories/EquipmentRepository.java
+++ backend/AdminApplication/src/app/repositories/EquipmentRepository.java
@@ -3,9 +3,9 @@ package app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import app.entities.Equipment;
+import app.entities.EquipmentRequest;
 
 @Repository
-public interface EquipmentRepository extends JpaRepository<Equipment, Long>  {
+public interface EquipmentRepository extends JpaRepository<EquipmentRequest, Long>  {
 
 }
